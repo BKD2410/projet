212b35ce022c20613b119802de40feafb306a105
BKD2410 <dienebetty5@gmail.com>
2025-07-04 16:20:57 +0000

    modification de khady

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ def bonjour(nom):
 
 if __name__ == "__main__":
     user = input("Entrez votre nom : ")
-    print("bonjour",user,",bienvenue! ")
+    print("bonjour",user,",bienvenue a l'ESMT")
 
-def dire_aurevoir(nom):
-    print(f"Au revoir, {nom} !")
-if __name__ == "__main__":
-    nom = input("Ton nom : ")
-    dire_bonjour(nom)
-    dire_aurevoir(nom)
+
 
